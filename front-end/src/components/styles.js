92ce34e5839226ef775ef1b671a0999804f0084f
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2025-05-28 21:45:35 -0300

    fix: adjust height properties in styled components for better layout consistency

--- front-end/src/components/styles.js
+++ front-end/src/components/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: fit-content;
-  height: 100vh;
+  height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
