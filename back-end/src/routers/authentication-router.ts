60fd22f44946bcab5d22b5c4dd88e1869b6e60b9
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2025-12-11 20:58:01 -0300

    feat: add Swagger documentation for authentication and user creation endpoints

--- back-end/src/routers/authentication-router.ts
+++ back-end/src/routers/authentication-router.ts
@@ -5,6 +5,44 @@ import { signInSchema } from '@/schemas';
 
 const authenticationRouter = Router();
 
+/**
+ * @swagger
+ * /auth/sign-in:
+ *   post:
+ *     summary: Authenticate a user
+ *     tags: [Authentication]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 format: email
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *                 id:
+ *                   type: integer
+ *                 email:
+ *                   type: string
+ *       401:
+ *         description: Unauthorized
+ */
 authenticationRouter.post('/sign-in', validateBody(signInSchema), signInPost);
 
 export { authenticationRouter };
--- back-end/src/routers/users-router.ts
+++ back-end/src/routers/users-router.ts
@@ -6,6 +6,48 @@ import { usersPost } from '@/controllers';
 
 const usersRouter = Router();
 
+/**
+ * @swagger
+ * /users:
+ *   post:
+ *     summary: Create a new user
+ *     tags:
+ *       - Users
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 format: email
+ *               password:
+ *                 type: string
+ *                 minLength: 6
+ *     responses:
+ *       201:
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                 email:
+ *                   type: string
+ *       400:
+ *         description: Bad Request
+ *       409:
+ *         description: Conflict
+ *       422:
+ *         description: Unprocessable Entity
+ */
 usersRouter.post('/', validateBody(createUserSchema), usersPost);
 
 export { usersRouter };
